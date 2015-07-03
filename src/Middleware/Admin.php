1934460b0dfc0deed1cff029daf4a2c08381ce4c
Shawn Grover <sgrover@open2space.com>
2015-07-03 14:37:45 -0600

    added the initial security middleware object

---
+++ src/Middleware/Admin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace O2s\Users\Middleware;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        dd($request);
+
+        return $next($request);
+    }
+}
